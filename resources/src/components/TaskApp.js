1dba90260257755000f30be5db638d5ce98894b3
Justin de Leon <jusmdeleon@gmail.com>
2015-09-25 18:44:58 +0800

    Task app

---
+++ resources/src/components/TaskApp.js
@@ -0,0 +1,39 @@
+var TaskApp = new React.createClass({
+
+	getInitialState: function() {
+		return {
+			items: [],
+			task: '',
+		}
+	},
+
+	onChange: function(e) {
+		this.setState({
+			task: e.target.value
+		});
+	},
+
+	addTask: function(e) {
+		e.preventDefault();
+		this.setState({
+			items: this.state.items.concat([this.state.task]),
+			task: ''
+		});
+	},
+
+	render: function() {
+		return (
+			<div>
+				<h1>My Tasks</h1>
+				<TaskList items={this.state.items} />
+
+				<form onSubmit={this.addTask}>
+					<input onChange={this.onChange} value={this.state.task}/>
+					<button>Add Task</button>
+				</form>
+			</div>
+		)
+	}
+});
+
+React.render(<TaskApp />, document.body);
